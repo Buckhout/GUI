e1b9c0b235465af943d34bca0d6bd6006aedff15
Buckhout <70178872+Buckhout@users.noreply.github.com>
2020-11-18 16:31:25 -0600

    Added a frame and some buttons

--- GUITest.java
+++ GUITest.java
@@ -1,8 +1,36 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class GUITest {
 
+	private static final int HEIGHT = 400;
+	private static final int WIDTH = 400;
+
 	public static void main(String[] args)
 	{
+		// so far, all our programs have been sequential and they reached the END
+		
+		JFrame frame = new JFrame("Dark wave");
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
+		// Clash
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FlowLayout layout = new FlowLayout();
+		frame.setLayout(layout);
+		
+		JButton b1 = new JButton("Man who stare");
+		JButton b2 = new JButton("at goats");
+		JButton b52 = new JButton("at Code Monkeys");
+		JButton doNotPushMe = new JButton("my finger is on the button");
+		
+		frame.add(b1);
+		frame.add(b2);
+		frame.add(b52);
+		frame.add(doNotPushMe);
+		
+		frame.pack();
+		
+		frame.setVisible(true);
 		
 	}
 }
